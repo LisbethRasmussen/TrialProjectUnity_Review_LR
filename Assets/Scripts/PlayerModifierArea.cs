12b3076789761c0b22f5fbaee1b58e46a55a8c65
AdriKat2022 <adrien.schroedel91@gmail.com>
2025-03-01 11:16:14 +0100

    added particles for speed alteration effect

--- Assets/Scripts/PlayerModifierArea.cs
+++ Assets/Scripts/PlayerModifierArea.cs
@@ -3,12 +3,17 @@ using UnityEngine;
 public class PlayerModifierArea : MonoBehaviour
 {
     [SerializeField] private int _speedMultiplierModifier = 20;
+    [SerializeField] private ParticleSystem _particleEffect;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player"))
         {
             collision.GetComponent<IsometricMovementController>().SpeedMultiplier += _speedMultiplierModifier;
+            if (_particleEffect != null)
+            {
+                _particleEffect.Play();
+            }
         }
     }
 
@@ -17,6 +22,10 @@ public class PlayerModifierArea : MonoBehaviour
         if (collision.CompareTag("Player"))
         {
             collision.GetComponent<IsometricMovementController>().SpeedMultiplier -= _speedMultiplierModifier;
+            if (_particleEffect != null)
+            {
+                _particleEffect.Stop();
+            }
         }
     }
 }
