c258b13c860e655ff586e435153169ff17a4b2ae
AdriKat2022 <adrien.schroedel91@gmail.com>
2025-03-01 14:14:51 +0100

    added health manager script enemy now deals damage to player

--- Assets/Scripts/Enemies/AttackEnemyState.cs
+++ Assets/Scripts/Enemies/AttackEnemyState.cs
@@ -1,10 +1,27 @@
+
+using UnityEngine;
+
 public class AttackEnemyState : EnemyState
 {
+    private HealthManager _playerHealthManager;
+    private bool _isDamageable = false;
+    private float _attackTimer = 0;
+
     public AttackEnemyState(EnemyBehaviour enemy) : base(enemy) { }
 
     public override void OnEnter()
     {
+        if (_enemy.ChasingTransform.TryGetComponent(out HealthManager healthManager))
+        {
+            _playerHealthManager = healthManager;
+            _isDamageable = true;
+        }
+        else
+        {
+            Debug.LogWarning("No health manager attached to the chased game object.");
+        }
 
+        _attackTimer = _enemy.AttackCooldown / 2f;
     }
 
     public override void OnExit()
@@ -14,6 +31,20 @@ public class AttackEnemyState : EnemyState
 
     public override void Update()
     {
+        if (_enemy.ChasingTransform == null || Vector3.Distance(_enemy.transform.position, _enemy.ChasingTransform.position) > _enemy.AttackRange)
+        {
+            _enemy.ChangeState(_enemy.ChaseState);
+            return;
+        }
 
+        if (_attackTimer < 0)
+        {
+            if (_isDamageable)
+            {
+                _playerHealthManager.DealDamage((int)_enemy.AttackDamage);
+            }
+            _attackTimer = _enemy.AttackCooldown;
+        }
+        _attackTimer -= Time.deltaTime;
     }
 }
