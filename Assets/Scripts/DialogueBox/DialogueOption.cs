5dc2aeaa5da0b94b4507851a9a635f1e4e8947b3
AdriKat2022 <adrien.schroedel91@gmail.com>
2025-03-03 21:00:07 +0100

    dialogue mechanic works with twine and Knitting!

---
+++ Assets/Scripts/DialogueBox/DialogueOption.cs
@@ -0,0 +1,28 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class DialogueOption : MonoBehaviour, IPointerClickHandler
+{
+    [SerializeField] private TextMeshProUGUI _optionText;
+
+    private DialogueManager _parentDialogueManager;
+    private int _id = 0;
+
+    public void MakeOption(string optionText, int id, DialogueManager parentDialogueManager)
+    {
+        _parentDialogueManager = parentDialogueManager;
+        _optionText.text = optionText;
+        _id = id;
+    }
+
+    private void OnClick()
+    {
+        _parentDialogueManager.OptionSelected(_id);
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        OnClick();
+    }
+}
