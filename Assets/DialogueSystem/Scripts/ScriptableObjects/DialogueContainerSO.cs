6ee4c1039c09dd78d4976f727ee7aab5e9c5a55d
AdriKat2022 <adrien.schroedel91@gmail.com>
2025-03-04 20:34:27 +0100

    the editor graph can now SAVE !!

--- Assets/DialogueSystem/Scripts/ScriptableObjects/DialogueContainerSO.cs
+++ Assets/DialogueSystem/Scripts/ScriptableObjects/DialogueContainerSO.cs
@@ -4,14 +4,14 @@ using UnityEngine;
 public class DialogueContainerSO : ScriptableObject
 {
     public string FileName { get; set; }
-    //public SerializableDictionary<DialogueGroupSO, List<DialogueSO>> DialogueGroups { get; set; }
+    public SerializableDictionary<DialogueGroupSO, List<DialogueSO>> DialogueGroups { get; set; }
     public List<DialogueSO> UngroupedDialogues { get; set; }
 
 
     public void Initialize(string fileName)
     {
         FileName = fileName;
-        //DialogueGroups = new SerializableDictionary<DialogueGroupSO, List<DialogueSO>>();
+        DialogueGroups = new SerializableDictionary<DialogueGroupSO, List<DialogueSO>>();
         UngroupedDialogues = new List<DialogueSO>();
     }
 }
--- Assets/Editor/DialogueSystem/Data/Save/DialogueGraphSaveDataSO.cs
+++ Assets/Editor/DialogueSystem/Data/Save/DialogueGraphSaveDataSO.cs
@@ -1,10 +1,10 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class DialogueGraphSaveData : ScriptableObject
+public class DialogueGraphSaveDataSO : ScriptableObject
 {
     [field: SerializeField] public string FileName { get; set; }
-    //[field: SerializeField] public List<DialogueGroupSaveData> Groups { get; set; }
+    [field: SerializeField] public List<DialogueGroupSaveData> Groups { get; set; }
     [field: SerializeField] public List<DialogueNodeSaveData> Nodes { get; set; }
     [field: SerializeField] public List<string> OldGroupNames { get; set; }
     [field: SerializeField] public List<string> OldUngroupedNodeNames { get; set; }
@@ -13,7 +13,7 @@ public class DialogueGraphSaveData : ScriptableObject
     public void Initialize(string fileName)
     {
         FileName = fileName;
-        //Groups = new List<DialogueGroupSaveData>();
+        Groups = new List<DialogueGroupSaveData>();
         Nodes = new List<DialogueNodeSaveData>();
     }
 }
