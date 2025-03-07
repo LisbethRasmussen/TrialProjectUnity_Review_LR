dc522fa033e5d65cf14f62697562af5376188817
AdriKat2022 <adrien.schroedel91@gmail.com>
2025-03-07 08:14:03 +0100

    created required attribute

--- Assets/Scripts/Attributes/ReadOnlyAttribute.cs
+++ Assets/Scripts/Attributes/ReadOnlyAttribute.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 [AttributeUsage(AttributeTargets.Property | AttributeTargets.Field, Inherited = false, AllowMultiple = true)]
 public class ReadOnlyAttribute : PropertyAttribute
 {
+    /// <summary>
+    /// Attribute to make a field read-only in the inspector.<br/>
+    /// The field won't be manually editable.
+    /// </summary>
     public ReadOnlyAttribute()
     {
     }
--- Assets/Scripts/Attributes/ShowIfAttribute.cs
+++ Assets/Scripts/Attributes/ShowIfAttribute.cs
@@ -8,6 +8,12 @@ public class ShowIfAttribute : PropertyAttribute
     public readonly bool DisableInsteadOfHidding;
     public readonly bool Invert;
 
+    /// <summary>
+    /// Attribute to show or hide a field in the inspector based on the value of a boolean variable.<br/>
+    /// </summary>
+    /// <param name="variableName">Name of the conditional variable.</param>
+    /// <param name="disableInsteadOfHidding">If the conditional variable is false, only disables the field instead of hiding it.</param>
+    /// <param name="invert">Invert the beheviour. Only show if the conditional variable is false instead of true.</param>
     public ShowIfAttribute(string variableName, bool disableInsteadOfHidding = false, bool invert = false)
     {
         VariableName = variableName;
